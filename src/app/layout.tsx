1acae25002ace323e15207a194f7b4b2fa101e14
Diego Añazco <danazco@vozy.co>
2023-10-29 17:47:38 -0500

    :sparkles: NEW FEATURE :: Home Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout ({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className='light bg-lightPurple-50'>
+    <html lang='en' className='light app'>
       <body className={inter.className}>
         <Providers>
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 'use client';
 
 import Button from './components/Button';
+import { Divider } from '@nextui-org/react';
 
 export default function Home () {
   return (
-    <main>
-      <Button typeStyle='secondary'>
-        Button
-      </Button>
+    <main className='max-w-screen-sm mx-auto h-screen flex flex-col justify-center items-center overflow-y-hidden'>
+      <h1 className='font-bold text-5xl text-center'>Notas de Fe</h1>
+      <h2 className='font-semibold text-2xl text-center'>
+        Cancionero de la Pastoral La Salle
+      </h2>
+      <Divider className='my-5 h-[1px] bg-black' />
+      <p className='text-base pb-5 text-center'>
+        Canciones que guían la labor de la Pastoral de La Salle. Estas
+        canciones, llenas de significado, están pensadas para alumnos, Hermanos
+        de La Salle, profesores y catequistas.
+      </p>
+      <div className='flex justify-center w-full'>
+        <Button typeStyle='primary'>
+          Explorar canciones
+        </Button>
+      </div>
     </main>
   );
 }
