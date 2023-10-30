02dd04e69a511c7ec72c8fa826165a602f7668c2
Diego Añazco <danazco@vozy.co>
2023-10-29 21:30:10 -0500

    :alien: WORDING :: Adding more songs

--- src/app/library/[song]/utils/constants.ts
+++ src/app/library/[song]/utils/constants.ts
@@ -119,5 +119,225 @@ export const songsList = [
       ['La única razón de mi adoración, eres tú mi Jesús.', 'Mí unico motivo para vivir eres tú mi Señor.', 'Mi única verdad está en ti, eres mi luz y mi salvación.', 'Mi único amor eres tu Señor, y por siempre te', 'alabaré.'],
       ['TÚ ERES TODO PODEROSO ERES GRANDE Y', 'MAJESTUOSO ERES FUERTE INVENCIBLE Y NO', 'HAY NADIE COMO TÚ.']
     ]
+  },
+  {
+    title: 'Gloria al Señor',
+    url: 'gloria-al-senor',
+    paragraphs: [
+      ['Gloria al Señor que está en el cielo, Gloria al Señor', 'que está en la tierra. Paz a los hombres de buena', 'voluntad. Te alabamos Señor, bendecimos tu amor, y', 'con tu dulce paz, danos tu bendición.'],
+      ['Hijo de Dios, eres Tú, Cristo, y estás sentado, a su', 'derecha. Paz a las almas que suplican perdón. Te', 'alabamos Señor, bendecimos tu amor; Santo', 'Espiritú, tu gracia danos hoy, tu gracia danos hoy,', 'tu gracia danos hoy.']
+    ]
+  },
+  {
+    title: 'Gloria Giombini',
+    url: 'gloria-giombini',
+    paragraphs: [
+      ['Gloria, gloria, a Dios en lo alto del cielo, gloria, y en', 'la tierra, y en la tierra, paz a los hombres que ama el', 'Señor.'],
+      ['TE ALABAMOS (2) TE BENDECIMOS (2)', 'TE ADORAMOS (2) TE GLORIFICAMOS (2)'],
+      ['Te damos, gracias, por tu inmensa gloria.'],
+      ['SEÑOR HIJO ÚNICO, JESUCRISTO, SEÑOR DIOS', 'CORDERO DE DIOS, HIJO DEL PADRE', 'TÚ QUE QUITAS EL PECADO, EL PECADO DEL', 'MUNDO, TEN PIEDAD DE NOSOTROS, TEN PIEDAD', 'DE NOSOTROS.'],
+      ['TÚ QUE ESTÁS A LA DERECHA, A LA DERECHA DEL', 'PADRE, TEN PIEDAD DE NOSOTROS (2)'],
+      ['Porque sólo Tú eres santo, sólo Tú Señor,', 'sólo Tú altísimo, Jesuscristo con el Espíritu Santo,'],
+      ['EN LA GLORIA DE DIOS PADRE, AMÉN']
+    ]
+  },
+  {
+    title: 'Hoy como ayer',
+    url: 'hoy-como-ayer',
+    paragraphs: [
+      ['Hace algún tiempo, brilló una propuesta que Dios', 'como estrella, al mundo envió, una pregunta exigía', 'respuesta: los pobres y su educación.'],
+      ['Días y meses velando por ella, así Juan Bautista al', 'fin se enamoró. Dijo ante el brillo de aquella', 'propuesta: "No puedo decirte que no".'],
+      ['HOY COMO AYER OYENDO LA VOZ DE DIOS, HOY LA', 'SALLE SOMOS TÚ Y YO. COMO LO HIZO ÉL', 'DIGAMOS QUE SÍ A DIOS. HOY LA SALLE SOMOS', 'TÚ Y YO.'],
+      ['Hoy es un tiempo colmado de estrellas que exigen de', 'urgencia, respuesta de amor. Dios sigue enviando la', 'misma propuesta, nosotros tenemos la voz.'],
+      ['Con esperanza, siguiendo sus huellas ser signos de fe', 'para el mundo de hoy. Como La Salle, nosotros', 'tampoco podemos decirle que no.'],
+      ['HOY COMO AYER OYENDO LA VOZ DE DIOS, HOY LA', 'SALLE SOMOS TÚ Y YO. COMO LO HIZO ÉL', 'DIGAMOS QUE SÍ A DIOS. HOY LA SALLE SOMOS', 'TÚ Y YO.']
+    ]
+  },
+  {
+    title: 'Iglesia joven',
+    url: 'iglesia-joven',
+    paragraphs: [
+      ['Somos como un torrente, que recién se está', 'formando y que mañana a más tardar, arrasará con', 'todo. Nos estamos preparando, para redimir al', 'mundo, y a ese hombre que está esclavo de si, que', 'forma la sociedad.'],
+      ['SOMOS LA IGLESIA JOVEN, TRATAMOS DE', 'TRANSFORMAR AL MUNDO ENTERO, EN UNA', 'SOCIEDAD LIMPIA Y JUSTA, DONDE NO EXISTA LA', 'ESCLAVITUD. NO NOS DETENDRÁN NADA NI NADIE,', 'SEREMOS LOS QUE ROMPAN LAS CADENAS, LAS', 'CADENAS QUE ATAN, A ESTE MUNDO SIN AMOR.'],
+      ['Será una guerra sin armas, pero si de muchos', 'cambios, al cual el hombre tendrá que enfrentar con', 'solo su conversión. Entonces será otra la sociedad', 'ideal, la que Cristo quiso en la tierra y predicó al mundo.'],
+      ['SOMOS LA IGLESIA JOVEN, TRATAMOS DE', 'TRANSFORMAR AL MUNDO ENTERO, EN UNA', 'SOCIEDAD LIMPIA Y JUSTA, DONDE NO EXISTA LA', 'ESCLAVITUD. NO NOS DETENDRÁN NADA NI NADIE,', 'SEREMOS LOS QUE ROMPAN LAS CADENAS, LAS', 'CADENAS QUE ATAN, A ESTE MUNDO SIN AMOR.']
+    ]
+  },
+  {
+    title: 'María de Nazareth',
+    url: 'maria-de-nazareth',
+    paragraphs: [
+      ["Dulce muchacha humilde de Palestina, a ti pa' ser", 'su madre Dios te eligió y cuando desde el cielo te', 'mandó un ángel para pedir tu consentimiento, tú le', 'dijiste: "Tu esclava soy" (bis)'],
+      ['POR ESO VOY A DARTE MI CORAZÓN,', 'Y CANTANDO REPETIRÉ TU NOMBRE MARÍA DE', 'NAZARETH. (BIS)'],
+      ['En aquél tallercito de carpintero. Dios aprendió el', 'oficio del buen José; y tú, yendo y viniendo de la', 'cocina, guardabas cosas dentro del alma, que le', 'sirvieron para después. (bis)'],
+      ['POR ESO VOY A DARTE MI CORAZÓN,', 'Y CANTANDO REPETIRÉ TU NOMBRE MARÍA DE', 'NAZARETH. (BIS)']
+    ]
+  },
+  {
+    title: 'Muévete en mí',
+    url: 'muevete-en-mi',
+    paragraphs: [
+      ['El Espíritu de Dios está en este lugar, el Espíritu de', 'Dios se mueve en este lugar.'],
+      ['Está aquí para consolar, está aquí para liberar, está', 'aquí para guiar, el Espíritu de Dios está aquí.'],
+      ['MUÉVETE EN MÍ, MUÉVETE EN MÍ, TOCA', 'MI MENTE Y MI CORAZÓN, LLENA MI VIDA DE TU', 'AMOR, MUÉVETE EN MI, DIOS ESPÍRITU MUÉVETE', 'EN MÍ.']
+    ]
+  },
+  {
+    title: 'Nadie te ama como yo',
+    url: 'nadie-te-ama-como-yo',
+    paragraphs: [
+      ['Cuanto he esperado este momento, cuanto he', 'esperado que estuvieras así, cuanto he esperado', 'que me hablaras, cuanto he esperado que vinieras a', 'mí.'],
+      ['Yo sé bien lo que has vivido. Yo sé bien porque has', 'llorado. Yo sé bien lo que has sufrido, pues de tu', 'lado no me he ido.'],
+      ['PUES NADIE TE AMA COMO YO,', 'PUES NADIE TE AMA COMO YO,', 'MIRA LA CRUZ ESTA ES MI MÁS GRANDE PRUEBA.', 'NADIE TE AMA COMO YO.'],
+      ['PUES NADIE TE AMA COMO YO,', 'PUES NADIE TE AMA COMO YO,', 'MIRA LA CRUZ, FUE POR TI,', 'FUE PORQUE TE AMO', 'NADIE TE AMA COMO YO.'],
+      ['Yo se bien lo que me dices, aunque a veces no me', 'hablas, yo se bien lo que sientes aunque nunca lo', 'compartas. Yo a tu lado he caminado, junto a ti', 'siempre he ido y aún a veces te he cargado, yo he sido', 'tu mejor amigo.'],
+      ['PUES NADIE TE AMA COMO YO,', 'PUES NADIE TE AMA COMO YO,', 'MIRA LA CRUZ ESTA ES MI MÁS GRANDE PRUEBA.', 'NADIE TE AMA COMO YO.'],
+      ['PUES NADIE TE AMA COMO YO,', 'PUES NADIE TE AMA COMO YO,', 'MIRA LA CRUZ, FUE POR TI,', 'FUE PORQUE TE AMO', 'NADIE TE AMA COMO YO.']
+    ]
+  },
+  {
+    title: 'Padre Amerindio',
+    url: 'padre-amerindio',
+    paragraphs: [
+      ['Quiero decirte Padre en Aymará,', 'quiero cantarte Padre en Araucano,', 'quiero mostrarte Padre mi otavalo,', 'quiero darte las gracias por mi Amerindia.'],
+      ['Quiero decirte fuerte que te amo en Quechua,', 'quiero sembrar tu reino con trigo Maya,', 'quiero darte mi vida como Azteca,', 'quiero adornar tu frente con oro Inca.'],
+      ['MI PADRE EN TU CORAZÓN YO ENCUENTRO MI', 'SINTONÍA, MI SINTONÍA, Y QUIERO CONSAGRAR', 'AHORA A MI PUEBLO TODO A MARÍA, TODO A', 'MARÍA.'],
+      ['Quiero decirte Padre en Guaraní', ' quiero cantarte Padre con mi alma Shuara,', 'quiero mostrarte Padre sierra y mares,', 'quiero darte las gracias por mi Amerindia.'],
+      ['Quiero decirte fuerte que te amo en Quechua,', 'quiero sembrar tu reino con trigo Maya,', 'quiero darte mi vida como Azteca,', 'quiero adornar tu frente con oro Inca.'],
+      ['MI PADRE EN TU CORAZÓN YO ENCUENTRO MI', 'SINTONÍA, MI SINTONÍA, Y QUIERO CONSAGRAR', 'AHORA A MI PUEBLO TODO A MARÍA, TODO A', 'MARÍA.']
+    ]
+  },
+  {
+    title: 'Que cante la vida',
+    url: 'que-cante-la-vida',
+    paragraphs: [
+      ['Había esperanza, rondaba el amor, tendiendo sus', 'alas, quería evitar a los hombres el dolor. Decía la', 'historia, no quiero contar ninguna locura, un sueño', 'que todos podemos realizar.'],
+      ['QUE SE ELEVEN LAS VOCES EN UNA CANCIÓN,', 'SE JUNTEN LAS MANOS, SE LOGRE LA UNIÓN.', 'QUE CANTE LA VIDA POR TODO RINCÓN,', 'QUE SE ABRAN CAMINOS Y SE ENCIENDA EL SOL.'],
+      ['Y están los que piensan en solo destruir, y están los', 'que creen que todo es en vano y que el mundo va a', 'morir. Y estamos nosotros para despertar, el sueño', 'perdido, el sueño que todos podemos realizar.'],
+      ['QUE SE ELEVEN LAS VOCES EN UNA CANCIÓN,', 'SE JUNTEN LAS MANOS, SE LOGRE LA UNIÓN.', 'QUE CANTE LA VIDA POR TODO RINCÓN,', 'QUE SE ABRAN CAMINOS Y SE ENCIENDA EL SOL.']
+    ]
+  },
+  {
+    title: 'Salmo 28',
+    url: 'salmo-28',
+    paragraphs: [
+      ['Rendid honor a Yahvé santos del Señor, gloría y', 'poder al que hace la creación. Rendid honor al', 'glorioso nombre del Señor y adoradle en su Santo', 'esplendor.'],
+      ['SOBRE LOS MARES RESUENA LA VOZ DE DIOS,', 'GLORIOSO ES DIOS SU VOZ HACE TEMBLAR, ES', 'PODEROSA LA VOZ DEL ALTÍSIMO. (ALELUYA)'],
+      ['SU VOZ DESGARRA LOS CEDROS DEL LÍBANO.', '(ALELUYA), LA VOZ DEL SEÑOR LANZA LLAMAS', 'DE FUEGO. (ALELUYA) SACUDE LOS BOSQUES', 'LA VOZ DEL ALTÍSIMO. (ALELUYA), Y EN SU', 'SANTUARIO GRITAN: "¡GLORIA!"'],
+      ['Sentado está Dios sobre la tempestad. Sentado está', 'Yahvé cual rey eterno. La fuerza da al que pone su', 'confianza en Él, Yahvé bendice su pueblo con paz.'],
+      ['SOBRE LOS MARES RESUENA LA VOZ DE DIOS,', 'GLORIOSO ES DIOS SU VOZ HACE TEMBLAR, ES', 'PODEROSA LA VOZ DEL ALTÍSIMO. (ALELUYA)'],
+      ['SU VOZ DESGARRA LOS CEDROS DEL LÍBANO.', '(ALELUYA), LA VOZ DEL SEÑOR LANZA LLAMAS', 'DE FUEGO. (ALELUYA) SACUDE LOS BOSQUES', 'LA VOZ DEL ALTÍSIMO. (ALELUYA), Y EN SU', 'SANTUARIO GRITAN: "¡GLORIA!"']
+    ]
+  },
+  {
+    title: 'Santo Giombini',
+    url: 'santo-giombini',
+    paragraphs: [
+      ['Santo, santo, santo es el Señor,', 'Dios del universo. (2)', 'El cielo y la tierra, están lleno de tu gloria. (2)'],
+      ['HOSANNA, HOSANNA, HOSANNA.', 'HOSANNA EN LO ALTO DEL CIELO.', 'HOSANNA EN LO ALTO DEL CIELO. (2)'],
+      ['Bendito es el que viene, en nombre del', 'Señor. (2)']
+    ]
+  },
+  {
+    title: 'Sentir su amor',
+    url: 'sentir-su-amor',
+    paragraphs: [
+      ['Cuando Cristo viene a tocar a la puerta de tu', 'corazón; siente en tus hermanos y en la creación su', 'voz.'],
+      ['Si quieres oír tienes que abrir el corazón, y sentirte', 'amado en medio de las fuerzas que nos regaló.'],
+      ['Y PODRÁS AYUDA BRINDAR Y SABRÁS DE LO QUE', 'ERES CAPAZ. LA FUERZA NO SE ACABA SI VIENE', 'DE SU AMOR.'],
+      ['TÚ VIENES A CUMPLIR UNA MISIÓN Y SIENTES A', 'CRISTO EN EL CORAZÓN PARA SERVIR A LOS', 'DEMÁS CON AMOR. (BIS)'],
+      ['Mira las montañas, que alaban su grandeza en el', 'verdor. Siente el viento fresco que repite su nombre', 'veloz.'],
+      ['Busca en la mirada de los más pequeños el perdón,', ' siente en sus caricias las manos de Dios en acción.'],
+      ['Y PODRÁS AYUDA BRINDAR Y SABRÁS DE LO QUE', 'ERES CAPAZ. LA FUERZA NO SE ACABA SI VIENE', 'DE SU AMOR.'],
+      ['TÚ VIENES A CUMPLIR UNA MISIÓN Y SIENTES A', 'CRISTO EN EL CORAZÓN PARA SERVIR A LOS', 'DEMÁS CON AMOR. (BIS)']
+    ]
+  },
+  {
+    title: 'Señor ten piedad',
+    url: 'senor-ten-piedad',
+    paragraphs: [
+      ['Señor ten piedad de nosotros,', 'de nosotros ten piedad.', 'Señor ten piedad de nosotros,', 'oh, oh, oh, Señor de nosotros ten piedad.'],
+      ['Cristo ten piedad...', 'Señor ten piedad...']
+    ]
+  },
+  {
+    title: 'Ser Lasallista hoy',
+    url: 'ser-lasallista-hoy',
+    paragraphs: [
+      ['Ser abeja y ser miel, ser sensible en la piel y', 'consciente en el corazón; trabajar y luchar por un', 'mundo de paz... Es ser lasallista hoy.'],
+      ['Un sendero trazar, de justicia social y ponerse a', 'caminar; con los pobres gastar la energía, el amor...', 'Es ser lasallista hoy.'],
+      ['APÓSTOL DEL AMOR, TESTIGO DEL SEÑOR, CIEN', 'CAMINOS DIBUJAR, MIL BATALLAS QUE GANAR.', 'RADIANTE DE JUVENTUD, CONSTRUCTOR DE LA', 'AMISTAR, CON CRISTO EN EL CORAZÓN,', 'ESO ES SER LASALLISTA HOY.'],
+      ['En familia vivir, la colmena construir, unidos en la', 'ilusión, la alegría tocar y unos ojos abrir... Es ser', 'lasallsita hoy.'],
+      ['Con amigos forjar un futuro mejor, la miseria', 'desterrar, la injusticia vencer y hacia el sol', 'caminar... Es ser lasallista hoy.'],
+      ['APÓSTOL DEL AMOR, TESTIGO DEL SEÑOR, CIEN', 'CAMINOS DIBUJAR, MIL BATALLAS QUE GANAR.', 'RADIANTE DE JUVENTUD, CONSTRUCTOR DE LA', 'AMISTAR, CON CRISTO EN EL CORAZÓN,', 'ESO ES SER LASALLISTA HOY.']
+    ]
+  },
+  {
+    title: 'Sopla sobre mí',
+    url: 'sopla-sobre-mi',
+    paragraphs: [
+      ['Sopla sobre mí, Señor, y renueva la faz de mi', 'corazón, porque soy tierra sin vida, si me falta tu', 'alegría, por eso te canto.'],
+      ['ESPÍRITU SANTO, VEN A MÍ. (X3)'],
+      ['Sopla sobre mí, Señor y derrama tu agua viva sobre', 'mi corazón, porque soy tierra reseca, si me falta tu', 'presencia, por eso te canto.'],
+      ['ESPÍRITU SANTO, VEN A MÍ. (X3)'],
+      ['Sopla sobre mí, Señor como viento que renueva', 'mi corazón, no se rompen mis cadenas, sin tu fuego', ' que me quema, por eso te canto.'],
+      ['ESPÍRITU SANTO, VEN A MÍ. (X3)']
+    ]
+  },
+  {
+    title: 'Texto del profeta Oseas',
+    url: 'texto-del-profeta-oseas',
+    paragraphs: [
+      ['Volved a mí, yo soy tu Dios y no tengáis ningún', 'temor. Yo te hablaré de amor y paz y te atraeré con', 'suavidad.'],
+      ['MUCHO HE ESPERADO TU REGRESO Y HOY YO TE', 'PROMETO DARTE MI PERDÓN. (BIS)'],
+      ['Convierte a mí tu corazón, y así hallarás tu', 'salvación. Yo borraré tu iniquidad y a mi tu voz ha', 'de alabar.'],
+      ['TODAS TUS LLAGAS CURARÉ Y ROCÍO YO SERÉ QUE', 'APAGUE TU MALDAD. (BIS)'],
+      ['Confía en mí pues Santo soy, me gozo en ser tu', 'salvador. Arraígate muy firme en mi y brotarás cual', 'dulce vid.'],
+      ['Y CON LOS CIELOS CANTARÁS MIS SENDAD DE', 'VERDAD, JUSTICIA, AMOR Y PAZ. (BIS)']
+    ]
+  },
+  {
+    title: 'Tu propuesta y yo',
+    url: 'tu-propuesta-y-yo',
+    paragraphs: [
+      ['Hoy al despertar, estabas a mi lado esperando el', 'momento justo para invitarme sorprendido y sin', 'comprender te pregunté ¿Quién eres? Me dijiste soy', 'tu Dios y quiero hablar contigo.'],
+      ['Que me estabas esperando y que quieres compartir', 'tu plan conmigo, me explicaste. Me propones ser', 'feliz sirviendo a todos sin medida aunque en el', 'camino encuentre rocas y también espinas.'],
+      ['¿QUÉ QUIERES DE MI? PORQUE NO LO SÉ, TENGO', 'MIEDO DE ABRIR LA PUERTA QUE TÚ ME', 'SEÑALAS. POR FAVOR, SEÑOR, LO DEBO PENSAR,', 'TU PROPUESTA ES UN GRAN DESAFIO Y NO', 'QUIERO FALLAR.'],
+      ['Me dijiste que no tema, que siempre estarás', 'conmigo, que sólo me pides ser sincero y tener', 'confianza, que las cosas se darán si así quieres tú', 'que lo sean.'],
+      ['¿DÓNDE ESTÁS JESÚS, COMO LLEGAR A TI, PARA', 'QUE ME HAS ELEGIDO? AUN NO PUEDO', 'COMPRENDERLO, SOLO SE QUE HAS PENETRADO EN', 'MI CUERPO Y LO SIENTO Y QUE ME IMPULSAS A', 'TOMAR TU AMOR Y NO SOLTARLO MÁS.']
+    ]
+  },
+  {
+    title: 'Virgen de la estrella',
+    url: 'virgen-de-la-estrella',
+    paragraphs: [
+      ['Oh Virgen de la Estrella, nosotros tu hijos estamos', 'aquí, para rogarte oh madre, nos des bendiciones', 'mil.'],
+      ['Oh Virgen de la Estrella, queremos ser siempre', 'reflejo de ti, que quien nos mire te vea, madre de', 'luz y bondad.'],
+      ['BRINDAR QUEREMOS UN AMOR SINCERO, A', 'NUESTRO AMIGO Y A QUIEN NO LO ES.', 'AYUDANOS MADRE TU QUE SUPISTE DAR AMOR.']
+    ]
+  },
+  {
+    title: 'Ya no eres pan y vino',
+    url: 'ya-no-eres-pan-y-vino',
+    paragraphs: [
+      ['Ya no eres pan y vino, ahora que eres cuerpo y', 'sangre, vives en mí. De rodillas yo caigo al', 'contemplar tú bondad ¿Cómo no te voy a adorar?'],
+      ['Mientras te pierdes en mis labios, tu gracia va', 'inundando todo mi corazón. Por esa paz que me', 'llena de alegría mi ser ¿Cómo no te voy a adorar?'],
+      ['SEÑOR JESÚS, MI SALVADOR, AMOR ETERNO', 'AMOR DIVINO. YA NO FALTA NADA, LO TENGO', 'TODO, TE TENGO A TI'],
+      ['Dueño y Rey del universo ¿Cómo puede ser posible', 'que busques mi amor? Tú tan grande y yo pequeño', 'y te fijas en mí ¿Cómo no te voy a adorar?'],
+      ['De rodillas, yo te pido, que el día cuando tú me', 'llames sea como hoy. Para mirarte a los ojos y', 'poderte decir que ¿Cómo no te voy a adorar?'],
+      ['SEÑOR JESÚS, MI SALVADOR, AMOR ETERNO', 'AMOR DIVINO. YA NO FALTA NADA, LO TENGO', 'TODO, TE TENGO A TI']
+    ]
+  },
+  {
+    title: 'Yo soy de los dos',
+    url: 'yo-soy-de-los-dos',
+    paragraphs: [
+      ['Por un sendero camino en busca de ti, Señor, quiero', 'llegar a tu encuentro para vivir junto a ti.', 'Que duro y largo el camino, cuánta piedra en el', 'andar. Pero queremos brindarte hasta la vida Señor.'],
+      ['HAY QUE SABER QUE POR TU CRUZ SE VIVE UNA', 'VIDA MEJOR, PERO SEÑOR, COMO NOS CUESTA', 'CREERLO CON EL CORAZÓN.', 'QUIERO, SEÑOR, VIVIR PARA TI Y SER DE LOS', 'DEMÁS. CON TU PALABRA Y MI CANSANCIO', 'HAREMOS LA VIDA CRECER.'],
+      ['Quiero cantarle esperanza a aquél que no tiene voz,', 'trabajo y pan en su mesa y en la de todos los pobres.', 'Canto por aquellos que siente el horizonte oscuro', 'y por auqellos que sufren en su cuerpo desnudo.'],
+      ['HAY QUE SABER QUE POR TU CRUZ SE VIVE UNA', 'VIDA MEJOR, PERO SEÑOR, COMO NOS CUESTA', 'CREERLO CON EL CORAZÓN.', 'QUIERO, SEÑOR, VIVIR PARA TI Y SER DE LOS', 'DEMÁS. CON TU PALABRA Y MI CANSANCIO', 'HAREMOS LA VIDA CRECER.'],
+      ['Miro a Dios y a la gente porque yo soy de los dos. De', 'su vida y su silencio yo haré mi canción. Como María', 'y el trigo desde el vientre dan la luz, quiero en mi vida', 'fecunda, nacer a tu entrega, Señor.'],
+      ['HAY QUE SABER QUE POR TU CRUZ SE VIVE UNA', 'VIDA MEJOR, PERO SEÑOR, COMO NOS CUESTA', 'CREERLO CON EL CORAZÓN.', 'QUIERO, SEÑOR, VIVIR PARA TI Y SER DE LOS', 'DEMÁS. CON TU PALABRA Y MI CANSANCIO', 'HAREMOS LA VIDA CRECER.']
+    ]
   }
 ];
