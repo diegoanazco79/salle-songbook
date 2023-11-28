d6cd2a9cbffdd7c4472df5ddff10ab4a65909864
Diego Añazco <danazco@vozy.co>
2023-11-27 22:02:12 -0500

    :bug: BUG FIX :: Fix when song no have a chords

--- src/app/library/[song]/components/Lyrics.tsx
+++ src/app/library/[song]/components/Lyrics.tsx
@@ -22,34 +22,55 @@ const Lyrics = ({ paragraphs, mode }: Props) => {
 
   return (
     <div className='text-center'>
-      {hasChords
-        ? (
-          <>
-            {paragraphs?.map((setOfParagraphs, setIndex) => (
-              <div className={setIndex !== 0 ? 'pt-4' : ''} key={setIndex}>
-                {setOfParagraphs?.map((paragraph, paragraphIndex) => (
-                  <div className={`${paragraph?.chords?.length > 0 && paragraphIndex === 0 ? 'mt-5' : ''} ${paragraph?.chords?.length > 0 && paragraphIndex !== setOfParagraphs?.length - 1 && mode === CHORDS_MODE ? 'h-[50px]' : ''}`} key={paragraphIndex}>
-                    {paragraph?.line?.split('').map((character, characterIndex) => {
-                      const chord = paragraph?.chords?.find(chord => characterIndex === chord.position);
-                      return (
-                        <span key={characterIndex} className={`text-xs md:text-base ${chord ? 'relative inline-block' : ''} ${isEntireLineChorus(paragraph?.line) ? 'font-semibold' : ''}`}>
-                          {chord && mode === CHORDS_MODE && <span className='absolute top-[-20px] font-semibold left-0'>{chord.chord}</span>}
-                          {mode === CHORDS_MODE && <span className={character === '&' ? 'text-transparent' : ''}>{character}</span>}
-                          {mode === LYRICS_MODE && <span>{character === '&' ? '' : character}</span>}
-                        </span>
-                      );
-                    })}
+      {mode === LYRICS_MODE &&
+        <>
+          {paragraphs?.map((setOfParagraphs, setIndex) => (
+            <div className={setIndex !== 0 ? 'pt-4' : ''} key={setIndex}>
+              {setOfParagraphs?.map((paragraph, paragraphIndex) => (
+                <div className={`${paragraph?.chords?.length > 0 && paragraphIndex === 0 ? 'mt-5' : ''}`} key={paragraphIndex}>
+                  {paragraph?.line?.split('').map((character, characterIndex) => {
+                    const chord = paragraph?.chords?.find(chord => characterIndex === chord.position);
+                    return (
+                      <span key={characterIndex} className={`text-xs md:text-base ${chord ? 'relative inline-block' : ''} ${isEntireLineChorus(paragraph?.line) ? 'font-semibold' : ''}`}>
+                        {mode === LYRICS_MODE && <span>{character === '&' ? '' : character}</span>}
+                      </span>
+                    );
+                  })}
+                </div>
+              ))}
+            </div>
+          ))}
+        </>}
+      {mode === CHORDS_MODE &&
+        <>
+          {hasChords
+            ? (
+              <>
+                {paragraphs?.map((setOfParagraphs, setIndex) => (
+                  <div className={setIndex !== 0 ? 'pt-4' : ''} key={setIndex}>
+                    {setOfParagraphs?.map((paragraph, paragraphIndex) => (
+                      <div className={`${paragraph?.chords?.length > 0 && paragraphIndex === 0 ? 'mt-5' : ''} ${paragraph?.chords?.length > 0 && paragraphIndex !== setOfParagraphs?.length - 1 && mode === CHORDS_MODE ? 'h-[50px]' : ''}`} key={paragraphIndex}>
+                        {paragraph?.line?.split('').map((character, characterIndex) => {
+                          const chord = paragraph?.chords?.find(chord => characterIndex === chord.position);
+                          return (
+                            <span key={characterIndex} className={`text-xs md:text-base ${chord ? 'relative inline-block' : ''} ${isEntireLineChorus(paragraph?.line) ? 'font-semibold' : ''}`}>
+                              {chord && mode === CHORDS_MODE && <span className='absolute top-[-20px] font-semibold left-0'>{chord.chord}</span>}
+                              {mode === CHORDS_MODE && <span className={character === '&' ? 'text-transparent' : ''}>{character}</span>}
+                            </span>
+                          );
+                        })}
+                      </div>
+                    ))}
                   </div>
                 ))}
-              </div>
-            ))}
-          </>
-        )
-        : (
-          <p className='text-center text-base font-semibold mt-4'>
-            No se encontraron acordes para esta canción
-          </p>
-        )}
+              </>
+            )
+            : (
+              <p className='text-center text-base font-semibold mt-4'>
+                No se encontraron acordes para esta canción
+              </p>
+            )}
+        </>}
     </div>
   );
 };
